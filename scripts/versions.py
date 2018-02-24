b58fbfc790bc5b27c41e2e93983f2504e12fb305
Anne Fraysse <afraysse@tetrascience.com>
2018-02-23 23:00:35 -0500

    visualization is working

---
+++ scripts/versions.py
@@ -0,0 +1,25 @@
+# Checks library versions
+
+# Python version
+import sys
+print('python: {}'.format(sys.version))
+
+# Scipy version
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+
+# Numpy version
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+
+# matplotlib version
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+
+# pandas version
+import pandas
+print('pandas: {}'.format(matplotlib.__version__))
+
+# scikit-learn version
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
